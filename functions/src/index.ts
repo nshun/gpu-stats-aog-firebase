ad6fb63a92d4e4c073b615c64193c74b5543f7a5
Shun Nishimura <n.shun1110@gmail.com>
2019-01-22 20:02:35 +0900

    Change: region to Tokyo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,10 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+const helloWorld = functions
+  .region('asia-northeast1')
+  .https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+  });
+
+exports.webhookAsia = helloWorld;

8f5346ffbbb4d664eaa603273ae9873661cefdd8
Shun Nishimura <n.shun1110@gmail.com>
2019-01-25 03:34:39 +0900

    Add: server images

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,29 +10,50 @@ const app = dialogflow({ debug: true });
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
+interface strStr {
+  [key: string]: string;
+}
+const serverImages: strStr = {
+  'neptune': 'https://upload.wikimedia.org/wikipedia/commons/5/56/Neptune_Full.jpg',
+  'uranus': 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Uranus2.jpg/220px-Uranus2.jpg',
+  'mercury': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/220px-Mercury_in_color_-_Prockter07-edit1.jpg',
+}
+
 app.intent('Default Welcome Intent', conv => {
   if (!conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT')) {
     conv.ask('Sorry, try this on a screen device or select the ' +
       'phone surface in the simulator.');
     return;
   }
-  conv.ask('調べたい GPU サーバーを選択してください')
+  conv.ask('調べたい GPU サーバーを選択してください');
   conv.ask(new Carousel({
     items: {
       "neptune": {
         title: 'neptune',
-        description: 'Description of number one',
-        synonyms: ['synonym of KEY_ONE 1', 'synonym of KEY_ONE 2'],
+        synonyms: ['ネプチューン'],
+        description: 'JupyterLab Server',
+        image: new Image({
+          url: serverImages.neptune,
+          alt: 'neptune',
+        }),
       },
       "uranus": {
         title: 'uranus',
-        description: 'Description of number one',
-        synonyms: ['synonym of KEY_TWO 1', 'synonym of KEY_TWO 2'],
+        synonyms: ['ウラヌス'],
+        description: 'JupyterLab Server',
+        image: new Image({
+          url: serverImages.uranus,
+          alt: 'uranus',
+        }),
       },
       "mercury": {
         title: 'mercury',
-        description: 'Description of number one',
-        synonyms: ['synonym of KEY_TWO 1', 'synonym of KEY_TWO 2'],
+        synonyms: ['マーキュリー'],
+        description: 'JupyterLab Server',
+        image: new Image({
+          url: serverImages.mercury,
+          alt: 'mercury',
+        }),
       },
     }
   }));
@@ -50,39 +71,19 @@ app.intent('actions.intent.OPTION', (conv, params, option) => {
         title: `${name}`,
         subtitle: `${data.gpuName} (${data.gpuTemp}℃) \n ${data.updatedAt}`,
         image: new Image({
-          url: 'https://avatars0.githubusercontent.com/u/23533486',
+          url: serverImages[name] || 'https://avatars0.githubusercontent.com/u/23533486',
           alt: 'Actions on Google'
         }),
         columns: [
-          {
-            align: 'CENTER',
-          },
-          {
-            header: 'current',
-            align: 'TRAILING',
-          },
-          {
-            header: 'max',
-            align: 'TRAILING',
-          },
+          { align: 'CENTER', },
+          { header: 'current', align: 'TRAILING', },
+          { header: 'max', align: 'TRAILING', },
         ],
         rows: [
-          {
-            cells: ['CPU', `${data.cpuUtil} %`, ''],
-            dividerAfter: false,
-          },
-          {
-            cells: ['メインメモリ', `${data.memUtil} %`, `${data.memTotal} MB`],
-            dividerAfter: true,
-          },
-          {
-            cells: ['GPU', `${data.gpuUtil} %`, ''],
-            dividerAfter: false,
-          },
-          {
-            cells: ['GPUメモリ', `${Math.round(data.gpuMemUsed / data.gpuMemTotal * 1000) / 10} %`, `${data.gpuMemTotal} MB`],
-            dividerAfter: true,
-          },
+          { cells: ['CPU', `${data.cpuUtil} %`, ''], dividerAfter: false, },
+          { cells: ['メインメモリ', `${data.memUtil} %`, `${data.memTotal} MB`], dividerAfter: true, },
+          { cells: ['GPU', `${data.gpuUtil} %`, ''], dividerAfter: false, },
+          { cells: ['GPUメモリ', `${Math.round(data.gpuMemUsed / data.gpuMemTotal * 1000) / 10} %`, `${data.gpuMemTotal} MB`], dividerAfter: true, },
         ],
         buttons: new Button({
           title: 'Kibana',
